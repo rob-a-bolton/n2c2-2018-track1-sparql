98ecbef8d5a0a6106dc666c6a26eb445b05d18cd
rob.a.bolton <rob.a.bolton@gmail.com>
2022-02-11 14:06:21 +0000

    Finished sql import

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname $(readlink -f $0))"
+ROOTDIR="$(pwd)"
 
 function print_usage {
   echo "./run.sh [OPTIONS] [ACTION]
@@ -8,6 +9,7 @@ function print_usage {
     -h          Print this help
     -u          Specify the SQL username
     -p          Specify the SQL password
+    -d          Specify the SQL database name
     -s          Specify the SPARQL endpoint
     -c          Clean. Wipes data associated with an action.
                 Examples: 
@@ -25,40 +27,66 @@ function print_usage {
 " >&2
 }
 
+function cleanup {
+  if [[ -f PGPASS ]]; then
+    rm PGPASS
+  fi
+}
+
+function failsafe {
+  echo "$1" >&2
+  cleanup
+  exit 1
+}
+
 function check_data_exists {
   if ! [[ -d ./data/train ]] || ! [[ -d ./data/n2c2-t1_gold_standard_test_data/n2c2-t1_gold_standard_test_data/test ]]; then
-    echo 'Could not locate n2c2 data, exiting' >&2
-    exit 1
+    failsafe 'Could not locate n2c2 data, exiting'
   fi
 }
 
 function setup_db_con_str {
-  if [[ -z "$USER" ]] || [[ -z "$PASS" ]]; then
-    echo 'Must specify both -u USER and -p PASS to perform an action that requires SQL' >&2
-    exit 1
+  if [[ -z "$USER" ]] || [[ -z "$PASS" ]] || [[ -z "$DBNAME" ]]; then
+    failsafe 'Must specify -u USER, -p PASS, and -d DBNAME to perform an action that requires SQL' >&2
   fi
-  DBSTR="jdbc:postgresql://localhost/n2c2?user=${USER}&password=${PASS}"
+  DBSTR="jdbc:postgresql://localhost/${DBNAME}?user=${USER}&password=${PASS}"
+  touch PGPASS
+  chmod 0600 PGPASS
+  echo "localhost:5432:${DBNAME}:${USER}:${PASS}" > PGPASS
+  export PGPASSFILE=PGPASS
 }
 
 function run_sql {
-  echo 'Importing n2c2 from XML to SQL'
   check_data_exists
   setup_db_con_str
-  pushd n2c22018t12sql
-  # TODO: Support -c via `psql`
-  if ! clj -M -m n2c22018t12sql.core -t ${PATIENT_TABLE} -a true -d ../data/train/ -j ${DBSTR}; then
-    echo 'Import failed, exiting'
-    exit 1
+  if [[ -z "${CLEAN}" ]]; then
+    echo 'Importing n2c2 from XML to SQL'
+    pushd n2c22018t12sql
+    if clj -M -m n2c22018t12sql.core -a true -d ../data/train/ -j ${DBSTR}; then
+      echo 'Import complete'
+    else
+      failsafe 'Import failed, exiting'
+    fi
+    popd
+  else
+    echo 'Cleaning n2c2 tables'
+    psql -h localhost -p 5432 -U ${USER} -d ${DBNAME} << "SQL"
+      DROP TABLE IF EXISTS patients CASCADE;
+      DROP TABLE IF EXISTS annotations CASCADE;
+      DROP TABLE IF EXISTS documents CASCADE;
+SQL
+    echo 'Tables cleaned'
   fi
-  popd
-  echo 'Import complete'
 }
 
-while getopts ':h:u:p:s:' OPTION; do
+while getopts ':hcu:p:d:s:' OPTION; do
   case ${OPTION} in
     h)
       print_usage
       exit 0
+      ;;
+    c)
+      CLEAN=yes
       ;;
     u)
       USER=${OPTARG}
@@ -66,12 +94,14 @@ while getopts ':h:u:p:s:' OPTION; do
     p)
       PASS=${OPTARG}
       ;;
+    d)
+      DBNAME=${OPTARG}
+      ;;
     s)
       SPARQL_ENDPOINT=${OPTARG}
       ;;
     ?)
-      echo "Invalid option: -${OPTARG}"
-      exit 1
+      failsafe "Invalid option: -${OPTARG}"
       ;;
   esac
 done
@@ -79,12 +109,12 @@ done
 ACTION=${@:$OPTIND:1}
 
 if [[ -z ${ACTION} ]]; then
-  echo 'No action specified' >&2
-  exit 1
+  failsafe 'No action specified' >&2
 elif [[ 'sql' == ${ACTION} ]]; then
   echo 'Inserting to SQL'
   run_sql
 else
-  echo "Invalid action: ${ACTION}" >&2
-  exit 1
+  failsafe "Invalid action: ${ACTION}" >&2
 fi
+
+cleanup
