04574c5ce3ab1976294c992d985ca5c4899bcc55
rob.a.bolton <rob.a.bolton@gmail.com>
2022-02-15 13:39:20 +0000

    Added stage2: MedCATservice annotation extraction

--- run.sh
+++ run.sh
@@ -5,27 +5,35 @@ ROOTDIR="$(pwd)"
 
 function print_usage {
   echo "./run.sh [OPTIONS] [ACTION]
+  Args marked REQUIRED/<TAG> denotes the arg tag name.
+  Actions specifying a tag need all args with this tag to be provided.
   Options:
     -h          Print this help
-    -u          Specify the SQL username
-    -p          Specify the SQL password
-    -d          Specify the SQL database name
-    -s          Specify the SPARQL endpoint
+    -u          Specify the SQL username        REQUIRED/SQL
+    -p          Specify the SQL password        REQUIRED/SQL
+    -d          Specify the SQL database name   REQUIRED/SQL
+    -s          Specify the SPARQL endpoint     REQUIRED/SPARQL
+    -m          Specify the MedCATservice endpoint
+                  Default: http://127.0.0.1:5000/api/process_bulk
     -c          Clean. Wipes data associated with an action.
                 Examples: 
                   ./run.sh -c sql       Drop sql n2c2 tables
                   ./run.sh -c medcat    Drop sql medcat annotation tables
                   ./run.sh -c umls      Drop the UMLS RDF graphs
                   ./run.sh -c rml       Drop the n2c2 RDF graphs
+
+  Tags following action show arg group needed.
   Actions:
-    sql         Loads the n2c2 data into a SQL database
-    medcat      Extracts medcat annotations and inserts into database
-    umls        Load UMLS subset into the RDF triple store
-    rml         Pulls annotated data from SQL into RDF, and applies any necessary patches
-    sparql      Runs the cohort 
-    run, all    Runs full pipeline (all actions)
+    sql         Loads the n2c2 data into a SQL database [SQL]
+    medcat      Extracts medcat annotations and inserts into database [SQL]
+    umls        Load UMLS subset into the RDF triple store [SPARQL]
+    rml         Pulls annotated data from SQL into RDF, and applies any necessary patches [SQL,SPARQL]
+    sparql      Runs the cohort [SPARQL]
+    run, all    Runs full pipeline (all actions) [SQL,SPARQL]
 " >&2
 }
+
+MEDCAT_ENDPOINT='http://127.0.0.1:5000/api/process_bulk'
 
 function cleanup {
   if [[ -f PGPASS ]]; then
@@ -79,7 +87,33 @@ SQL
   fi
 }
 
-while getopts ':hcu:p:d:s:' OPTION; do
+function run_medcat {
+  check_data_exists
+  setup_db_con_str
+  if [[ -z "${CLEAN}" ]]; then
+    pushd ann2sql
+    if clj -M -m ann2sql.core -s ${DBSTR} -d ${DBSTR} \
+       -S documents \
+       -D medcat \
+       -u ${MEDCAT_ENDPOINT} \
+       -c pat_id -c doc_id -c date -t text \
+       --create-tables;  then
+      echo 'MedCAT annotation succeeded'
+    else
+      failsafe 'MedCAT annotation failed, exiting'
+    fi
+    popd
+  else
+    echo 'Cleaning medcat tables'
+    psql -h localhost -p 5432 -U ${USER} -d ${DBNAME} << "SQL"
+      DROP TABLE IF EXISTS medcat
+SQL
+    echo 'Tables cleaned'
+
+  fi
+}
+
+while getopts ':hcu:p:d:m:s:' OPTION; do
   case ${OPTION} in
     h)
       print_usage
@@ -97,6 +131,9 @@ while getopts ':hcu:p:d:s:' OPTION; do
     d)
       DBNAME=${OPTARG}
       ;;
+    m)
+      MEDCAT_ENDPOINT=${OPTARG}
+      ;;
     s)
       SPARQL_ENDPOINT=${OPTARG}
       ;;
@@ -111,8 +148,9 @@ ACTION=${@:$OPTIND:1}
 if [[ -z ${ACTION} ]]; then
   failsafe 'No action specified' >&2
 elif [[ 'sql' == ${ACTION} ]]; then
-  echo 'Inserting to SQL'
   run_sql
+elif [[ 'medcat' == ${ACTION} ]]; then
+  run_medcat
 else
   failsafe "Invalid action: ${ACTION}" >&2
 fi
